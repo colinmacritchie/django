d6b2a77a02a6399bd1b0e77da29ee89997fe1cd9
Colin MacRitchie <ciscopowdercowboy@gmail.com>
2015-07-30 17:30:15 -0600

    updatyes

--- newsletter/forms.py
+++ newsletter/forms.py
@@ -1,6 +1,20 @@
 from django import forms
 from .models import SignUp
 
+class ContactForm(forms.Form):
+    full_name = forms.CharField(required=False)
+    email = forms.EmailField()
+    message = forms.CharField()
+
+def clean_email(self):
+    email = self.cleaned_data.get('email')
+    email_base, provider = email.split('@')
+    domain, extension = provider.split('.')
+    #if not extension == "gmail":    #Change this to whatever.
+        #raise forms.ValidationError("Please use a valid gmail email address") #Change message here.
+    return email
+
+
 class SignUpForm(forms.ModelForm):
     class Meta:
         model = SignUp
@@ -11,6 +25,6 @@ class SignUpForm(forms.ModelForm):
         email = self.cleaned_data.get('email')
         email_base, provider = email.split('@')
         domain, extension = provider.split('.')
-        if not extension == "gmail":    #Change this to whatever.
-            raise forms.ValidationError("Please use a valid gmail email address") #Change message here.
+        #if not extension == "gmail":    #Change this to whatever.
+            #raise forms.ValidationError("Please use a valid gmail email address") #Change message here.
         return email
--- newsletter/views.py
+++ newsletter/views.py
@@ -1,16 +1,42 @@
 from django.shortcuts import render
-from .forms import SignUpForm
+from .forms import SignUpForm, ContactForm
 # Create your views here.
 
+#Function that displays and saves the form.
 def home(request):
     title = "Welcome"
-    #if request.user.is_authenticated():
-        #title = "MVP %s" %(request.user)
-    if request.method == "POST":
-        print request.POST
-    form = SignUpForm()
+    form = SignUpForm(request.POST or None)
+
     context = {
-        "template_title": title,
+        "title": title,
         "form": form,
 }
+
+
+#Displays Thank you message after form is submitted.
+    if form.is_valid():
+        instance = form.save(commit=False)
+
+        full_name = form.cleaned_data.get("full_name")
+        if not full_name:
+            full_name = "New full name"
+        instance.full_name = full_name
+
+        instance.save()
+        context = {
+            "title": "Thank You!"
+
+        }
+
+#renders the request, view, and context.
     return render(request, "home.html", context)
+
+def contact(request):
+    form = ContactForm(request.POST or None)
+    if form.is_valid():
+        print form.cleaned_data
+
+    context = {
+        "form": form,
+    }
+    return render(request, "forms.html", context)
