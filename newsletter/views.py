bf3ee25d6c58c757d07412e63915be6e5099c524
Colin MacRitchie <ciscopowdercowboy@gmail.com>
2015-07-30 15:10:07 -0600

    adding forms

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-
+from .forms import SignUpForm
 # Create your views here.
 
 def home(request):
-    return render(request, "home.html", {})
+    title = "Welcome"
+    #if request.user.is_authenticated():
+        #title = "MVP %s" %(request.user)
+    if request.method == "POST":
+        print request.POST
+    form = SignUpForm()
+    context = {
+        "template_title": title,
+        "form": form,
+}
+    return render(request, "home.html", context)
