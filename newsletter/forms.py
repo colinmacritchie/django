3855c796e225d26609520acb24ecdba4ea915760
Colin MacRitchie <ciscopowdercowboy@gmail.com>
2015-07-30 14:30:03 -0600

    added gmail validation

--- newsletter/forms.py
+++ newsletter/forms.py
@@ -5,3 +5,11 @@ class SignUpForm(forms.ModelForm):
     class Meta:
         model = SignUp
         fields = ['full_name', 'email']
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        email_base, provider = email.split('@')
+        domain, extension = provider.split('.')
+        if not extension == "gmail":
+            raise forms.ValidationError("Please use a valud gmail email address")
+        return email
