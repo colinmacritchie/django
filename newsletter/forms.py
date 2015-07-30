a0334219d74a4630e0eb2a21b33300f4218d8766
Colin MacRitchie <ciscopowdercowboy@gmail.com>
2015-07-30 14:31:36 -0600

    added comments

--- newsletter/forms.py
+++ newsletter/forms.py
@@ -6,10 +6,11 @@ class SignUpForm(forms.ModelForm):
         model = SignUp
         fields = ['full_name', 'email']
 
+    #Delete this validation if you want to accept all email types.
     def clean_email(self):
         email = self.cleaned_data.get('email')
         email_base, provider = email.split('@')
         domain, extension = provider.split('.')
-        if not extension == "gmail":
-            raise forms.ValidationError("Please use a valud gmail email address")
+        if not extension == "gmail":    #Change this to whatever.
+            raise forms.ValidationError("Please use a valud gmail email address") #Change message here.
         return email
