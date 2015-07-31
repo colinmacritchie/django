ce30b455a8c709df50639ec27cf7b283167836fe
Colin MacRitchie <ciscopowdercowboy@gmail.com>
2015-07-31 12:47:00 -0600

    changes

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.core.mail import send_mail
 from django.shortcuts import render
 from .forms import SignUpForm, ContactForm
 # Create your views here.
@@ -36,8 +38,22 @@ def home(request):
 def contact(request):
     form = ContactForm(request.POST or None)
     if form.is_valid():
-        for key, value in form.cleaned_data.iteritems():
-            print key, value
+        email = form.cleaned_data.get("email")
+        message = form.cleaned_data.get("message")
+        full_name = form.cleaned_data.get("full_name")
+        subject = 'Site contact form'
+        from_email = settings.EMAIL_HOST_USER
+        to_email = from_email
+        contact_message = "%s: %s via %s"%(
+        full_name,
+        message,
+        email)
+
+        send_mail(subject,
+            contact_message,
+            from_email,
+            [to_email],
+            fail_silently=False)
 
     context = {
         "form": form,
--- trydjango18/settings.py
+++ trydjango18/settings.py
@@ -28,6 +28,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'youremail@gmail.com'
+EMAIL_HOST_PASSWORD = 'YourPassWordHere'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
 # Application definition
 
