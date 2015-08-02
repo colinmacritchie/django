aede2ac0f1c2a0e909f0c91a9a40f825d50eab71
Colin MacRitchie <ciscopowdercowboy@gmail.com>
2015-08-01 22:45:04 -0600

    changes

--- newsletter/models.py
+++ newsletter/models.py
@@ -11,3 +11,19 @@ class SignUp(models.Model):
 
     def __unicode__(self): #Python 3.3 is __str__
         return self.email
+
+
+class ApplicationNew(models.Model):
+    full_name = models.CharField(max_length=120, blank=True, null=True)
+    email = model.EmailField()
+    description = models.CharField(max_length=1000, blank=True, null=False)
+    description2 = models.CharField(max_length=600, blank=True, null=False)
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    address = models.CharField(max_length=50, blank=True, null=False)
+    zipcode = models.CharField(max_length=5, blank=True, null=False)
+    state = models.CharField(max_length=10, blank=True, null=False)
+    city = models.CharField(max_length=50, blank=True, null=False)
+    country = models.CharField(max_length=50, blank=True, null=False)
+    
+    def __unicode__(self): #Python 3.3 is __str__
+        return self.full_name
