573baa54152672d7b3f6c4cd9fb38a9e277b7e34
Colin MacRitchie <ciscopowdercowboy@gmail.com>
2015-08-04 14:43:41 -0600

    updates

--- trydjango18/settings.py
+++ trydjango18/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'newsletter',
 )
 
@@ -118,3 +119,5 @@ STATICFILES_DIRS = (
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_in_env", "media_root")
+
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
